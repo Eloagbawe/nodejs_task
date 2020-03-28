1b4d3d39bfaf9ae52b5bfe01e6ad03e1e2af509e
Eloagbawe <eloagbawe@gmail.com>
2020-03-28 13:43:54 +0100

    modified index.js

--- index.js
+++ index.js
@@ -1,62 +1,49 @@
 const http = require('http');
 const fs = require('fs');
-const { parse } = require('querystring')
-
+const {
+  parse
+} = require('querystring')
 const hostname = '127.0.0.1';
 const port = 8080;
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-
-  
-  // 
-  
-  
   fs.readFile('./form.html', null, (error, data) => {
     if (error) {
       res.setHeader(404);
       res.write("Not Found")
     } else {
       res.write(data)
-      
     }
     res.end();
   })
-  if (req.method === 'POST'){
-   collectRequestData (req, result => {
-        console.log(result)
-     })
-
+  if (req.method === 'POST') {
+    collectRequestData(req, result => {
+      console.log(result)
+    })
   }
 });
-
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-  
 });
-
-function collectRequestData (request, callback)  {
+function collectRequestData(request, callback) {
   const FORM_URLENCODED = 'application/x-www-form-urlencoded';
-  if(request.headers['content-type'] === FORM_URLENCODED){
+  if (request.headers['content-type'] === FORM_URLENCODED) {
     var body = '';
     request.on('data', (chunk) => {
       body += chunk
-      fs.writeFile("message.txt", body, (err) => {
+      var newBody = parse(body);
+      var message = newBody.message;
+      fs.appendFile("message.txt", `${message} \n`, (err) => {
         if (err) {
           console.log(err)
         }
-        console.log(parse(body));
+        console.log(newBody.message);
       })
-
       if (body.length > 1e6)
-                request.connection.destroy();
+        request.connection.destroy();
     });
-
-    
-   
-  }
-  else {
+  } else {
     callback(null)
   }
 }
